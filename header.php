15ebdbc7aae1db8d5fac44a6452bb786653836b3
Hyper2059 <aayuu2059@gmail.com>
2025-12-07 12:47:00 +0545

    Add files via upload

---
+++ header.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $title ?? "Home Page";?> | Ecommerce Website</title>
+    <link rel="stylesheet" href="static/css/style.css">
+</head>
+<body>
+    <nav>
+        <a href="/ecom/">Home</a> | 
+        <a href="/ecom/login.php">Login</a>
+    </nav>
+
+
